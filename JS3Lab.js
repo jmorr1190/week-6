588469f73777981eac8e5c36950b3ddf0f3fa9a2
jmorr1190 <jmorr1190@gmail.com>
2023-11-09 06:03:06 -0800

    finished q5

--- JS3Lab.js
+++ JS3Lab.js
@@ -168,6 +168,13 @@ Question 5: isEven  \n`);
  * ↓ YOUR CODE HERE ↓ */
 
 
+function isEven (n) {
+       return n % 2 == 0;
+}             console.log(isEven(2));
+              console.log(isEven(3));   
+              console.log(isEven(4));   
+              console.log(isEven(5)); 
+
 
 
 
